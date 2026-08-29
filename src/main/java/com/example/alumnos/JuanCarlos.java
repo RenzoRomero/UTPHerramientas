4bc882b83c0deb2c20820e7bd7ffc1d90a855b08
JuanCarlosCalderon <juancarloscalderonbendezu@gmail.com>
2026-08-28 19:20:36 -0500

    primer commit de clases

--- src/main/java/com/example/alumnos/JuanCarlos.java
+++ src/main/java/com/example/alumnos/JuanCarlos.java
@@ -1,6 +1,6 @@
 package com.example.alumnos;
-
 public class JuanCarlos {
-    Sytem.out.print("mi primera clase en salon")
-
+    public static void main(String[] args) {
+        System.out.println("primer commit");
+    }
 }
